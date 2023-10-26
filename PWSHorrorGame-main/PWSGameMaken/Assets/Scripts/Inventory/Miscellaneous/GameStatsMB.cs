36c37249ad2b9676c00c3fa2354451ac826eaed5
ShowLagg <showlagggithub@gmail.com>
2023-10-27 01:35:42 +0200

    Items droppen. verkeerde hoek, verkeerde kracht

--- PWSHorrorGame-main/PWSGameMaken/Assets/Scripts/Inventory/Miscellaneous/GameStatsMB.cs
+++ PWSHorrorGame-main/PWSGameMaken/Assets/Scripts/Inventory/Miscellaneous/GameStatsMB.cs
@@ -7,13 +7,6 @@ using UnityEngine;
 public class GameStatsMB : MonoBehaviour
 {
     #region Variables
-    public GameObject groundItemPrefab;
     public Transform collectables;
     #endregion
 }
-
-//Als een soort prullenbak opgeslagen, als er errors zijn, kan het zijn dat 1 van deze variabelen terug moet
-//public GameObject tempItemPrefab;
-//public Transform canvas;
-//public ItemDatabaseSO ItemDatabaseSO;
-//public GameObject descriptionPrefab;
