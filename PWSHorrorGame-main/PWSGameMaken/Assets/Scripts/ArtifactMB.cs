ffa5f01cf09312897fdab1361c75b6897130ecdd
ShowLagg <showlagggithub@gmail.com>
2023-10-28 01:17:48 +0200

    Camera Shake toegevoegd (Artifact werkt niet)

--- PWSHorrorGame-main/PWSGameMaken/Assets/Scripts/ArtifactMB.cs
+++ PWSHorrorGame-main/PWSGameMaken/Assets/Scripts/ArtifactMB.cs
@@ -1,15 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class ArtifactMB : MonoBehaviour
 {
 	[SerializeField] private GameObject rocksToSpawn;
 	[SerializeField] private GameObject wallToCollapse;
+
+	[SerializeField] private float shakeIntensity = 25f;
+	[SerializeField] private float shakeFrequency = 10f;
+	[SerializeField] private float shakeTime = 1f;
+	public CameraShake cameraShake;
 	private void OnTriggerEnter(Collider other)
 	{
 		if(other.gameObject.CompareTag("Player"))
 		{
-			rocksToSpawn.SetActive(true);
-			wallToCollapse.SetActive(false);
+			CinemachineShake.Instance.ShakeCamera(shakeIntensity, shakeFrequency, shakeTime);
+			Invoke("CollapseMap", shakeTime / 2);
 		}	
+	}
+
+	public void CollapseMap()
+	{
+		rocksToSpawn.SetActive(true);
+		wallToCollapse.SetActive(false);
 	}
 }
---
+++ PWSHorrorGame-main/PWSGameMaken/Assets/Scripts/CinemachineShake.cs
@@ -0,0 +1,82 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemachineShake : MonoBehaviour
+{
+	public static CinemachineShake Instance { get; private set; }	
+
+	private	CinemachineVirtualCamera  cinemachineVirtualCamera;
+	private float shakeTimerTotal;
+	private float shakeTimer;
+	private float startingIntensity;
+
+	private float defaultIntensity;
+	private float defaultFrequency;
+
+	private bool _hasClimaxed = false;
+
+	private void Awake()
+	{
+		Instance = this;
+		cinemachineVirtualCamera  = GetComponent<CinemachineVirtualCamera>();
+	}
+
+	public void ShakeCamera(float intensity, float frequency, float time)
+	{
+		CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+			cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+		
+		defaultIntensity = cinemachineBasicMultiChannelPerlin.m_AmplitudeGain;
+		defaultFrequency = cinemachineBasicMultiChannelPerlin.m_FrequencyGain;
+		
+		cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+		cinemachineBasicMultiChannelPerlin.m_FrequencyGain = frequency;
+
+		startingIntensity = intensity;
+		shakeTimerTotal = time;
+		//shakeTimer = time;
+	}
+
+	private void Update()
+	{
+		if(shakeTimerTotal > 0)
+		{
+			if(_hasClimaxed == false)
+			{
+				shakeTimer += Time.deltaTime;
+				CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+					cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+				cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = Mathf.Lerp(startingIntensity, 0f, 1- (shakeTimer / shakeTimerTotal));
+
+				if (shakeTimer >= shakeTimerTotal)
+				{
+					_hasClimaxed = true;
+				}
+
+			}
+			else if(_hasClimaxed == true)
+			{
+				shakeTimer -= Time.deltaTime;
+				CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+					cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+				cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = Mathf.Lerp(startingIntensity, 0f, 1 - (shakeTimer / shakeTimerTotal));
+				if(shakeTimer <= 0f)
+				{
+					ResetVariables(cinemachineBasicMultiChannelPerlin);
+				}
+			}
+		}
+	}
+
+	private void ResetVariables(CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin)
+	{
+		shakeTimerTotal = 0f;
+		_hasClimaxed = false;
+		cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = defaultIntensity;
+		cinemachineBasicMultiChannelPerlin.m_FrequencyGain = defaultFrequency;
+	}
+}
