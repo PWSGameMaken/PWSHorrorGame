d952a40228d92b0c5fe70f6579929f81a55957cd
ShowLagg <showlagggithub@gmail.com>
2024-01-06 12:09:07 +0100

    hintText update

--- PWSHorrorGame-main/PWSGameMaken/Assets/Scripts/Objects/CollectionPoint/CollectionPointMB.cs
+++ PWSHorrorGame-main/PWSGameMaken/Assets/Scripts/Objects/CollectionPoint/CollectionPointMB.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public abstract class CollectionPointMB : InteractableObjectMB
 {
+	private string _originalHintText;
+	[SerializeField] private string _isFullHintText;
 	protected bool isFull;
+
+	private void Start()
+	{
+		_originalHintText = hintText;
+	}
+
+	public void ChangeHintText()
+	{
+		if (isFull) { hintText = _originalHintText; }
+		if(!isFull) { hintText = _isFullHintText; }
+	}
 }
